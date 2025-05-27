16e796d8e5a9d2a1e42596005189fad8f0207b9d
O-A5M <akhmouchothman@gmail.com>
2025-05-27 07:18:56 +0100

    adding some functions

---
+++ philo/philo_routine.c
@@ -0,0 +1,17 @@
+#include "philo.h"
+#include <bits/types/struct_timeval.h>
+#include <sys/time.h>
+
+void	philo_sleep(int sleep_duration)
+{
+	struct timeval	start_time;
+
+	gettimeofday(&start_time, NULL);
+	usleep (sleep_duration);
+	printf("%d\n", start_time.tv_usec/1000);
+}
+
+int	main()
+{
+	philo_sleep(100);
+}
